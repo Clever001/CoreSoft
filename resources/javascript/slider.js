3db0779ed8d4eb30ea8d54a5243fbcedc1a5aa6a
Nikon Vitaly <cakevit@gmail.com>
2024-11-26 13:08:50 +0500

    Javascript is replaced.

---
+++ resources/javascript/slider.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.slider-body', {
+    loop_time_out: 1,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    });
